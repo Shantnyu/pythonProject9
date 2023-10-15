6ef25dfc00dd74b7a73cebff94c12054294792f8
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-10-15 18:51:17 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -50,8 +50,8 @@ salt = b'Tandon'  # Remember it should be a byte-object
 password = 'sb9166@nyu.edu'
 input_string = 'AlwaysWatching'
 
-encrypted_value = encrypt_with_aes(input_string, password, salt)
-encrypted_string = str(encrypted_value, 'utf-8')
+encrypted_value = encrypt_with_aes(input_string, password, salt) # test function
+decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # test function
 
 # For future use
 def generate_sha256_hash(input_string):
