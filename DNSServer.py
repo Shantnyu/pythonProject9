63bd7eee6e63485cd9dfd9aa97ed5665c07fc33d
Shantnyu <144965598+Shantnyu@users.noreply.github.com>
2023-10-15 17:43:11 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -11,14 +11,13 @@ import threading
 import signal
 import os
 import sys
-
 import hashlib
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import base64
+import ast
 
-# Encryption and Decryption Functions
 def generate_aes_key(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -42,57 +41,71 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data)
     return decrypted_data.decode('utf-8')
 
-salt = b'Tandon'  # Remember it should be a byte-object
-password = 'sb9166@nyu.edu'
-secret_data = 'AlwaysWatching'
+# Set salt, password, and input_string
+salt = b'Tandon'
+password = 'your_NYU_email@example.com'
+input_string = 'AlwaysWatching'
 
-# Encrypt secret data
-encrypted_secret_data = encrypt_with_aes(secret_data, password, salt)
-
-# Prepare DNS records
+# Create DNS records
 dns_records = {
     'safebank.com.': {
-        dns.rdatatype.A: '192.168.1.102'
+        dns.rdatatype.A: '192.168.1.102',
     },
     'google.com.': {
-        dns.rdatatype.A: '192.168.1.103'
+        dns.rdatatype.A: '192.168.1.103',
     },
     'legitsite.com.': {
-        dns.rdatatype.A: '192.168.1.104'
+        dns.rdatatype.A: '192.168.1.104',
     },
     'yahoo.com.': {
-        dns.rdatatype.A: '192.168.1.105'
+        dns.rdatatype.A: '192.168.1.105',
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: (encrypted_secret_data,)
+        dns.rdatatype.TXT: encrypt_with_aes(input_string, password, salt),
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
     }
 }
 
-# DNS server logic
 def run_dns_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', 53))
+    server_socket.bind(('127.0.0.1', 53))
 
     while True:
         try:
             data, addr = server_socket.recvfrom(1024)
             request = dns.message.from_wire(data)
             response = dns.message.make_response(request)
+
             question = request.question[0]
             qname = question.name.to_text()
             qtype = question.rdtype
 
             if qname in dns_records and qtype in dns_records[qname]:
                 answer_data = dns_records[qname][qtype]
-                rdata = dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)
-                response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
-                response.answer[-1].add(rdata)
 
-            response.flags |= dns.flags.AA
+                rdata_list = []
 
+                if qtype == dns.rdatatype.MX:
+                    for pref, server in answer_data:
+                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
+                elif qtype == dns.rdatatype.SOA:
+                    mname, rname, serial, refresh, retry, expire, minimum = answer_data
+                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum)
+                    rdata_list.append(rdata)
+                else:
+                    if isinstance(answer_data, str):
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
+                    else:
+                        rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+                for rdata in rdata_list:
+                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
+                    response.answer[-1].add(rdata)
+
+            response.flags |= 1 << 10
             server_socket.sendto(response.to_wire(), addr)
-            print("Responding to request:", qname)
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
